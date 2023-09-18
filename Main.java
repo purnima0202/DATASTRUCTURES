bb3a9fcce3fb9856efa72342006673dd6d03021f
purely purnima <108063726+purnima0202@users.noreply.github.com>
2023-09-18 11:45:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {// this is a block
+
+    public static void main(String[] args) {
+        int AGE=20;
+        long AGELong=AGE;
+        int secondMarks=21;
+        long hello=45;
+        int helloInt=(int)hello;
+        boolean isPassed= true;
+        float pi=3.14f;
+        char letter='A';
+        char symbol='@';
+        System.out.println("hello himanshu");
+        System.out.println(AGE);
+        System.out.println(secondMarks);
+        System.out.println(isPassed);
+        System.out.println(pi);
+        System.out.println('A');
+        System.out.println('@');
+        System.out.println(AGELong);
+        System.out.println(helloInt);//typecasting
+    }
+}
